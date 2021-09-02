006c07984478073968d1ea4ab521725eaee9f947
Thomas Fowler <ThomasJamesFowler97@gmail.com>
2021-09-02 10:41:06 +1000

    Added backing fields for all Shared & State flows, as well as url creation based off of the room code

--- src/main/kotlin/com/tfowl/cosmobuzz/CosmoBuzz.kt
+++ src/main/kotlin/com/tfowl/cosmobuzz/CosmoBuzz.kt
@@ -1,22 +1,52 @@
 package com.tfowl.cosmobuzz
 
 import io.socket.client.Socket
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 
 
 class CosmoBuzzRoom(val code: String, private val socket: Socket) {
-    val url: String = TODO()
+    val url: String = URL_FMT.format(code)
 
-    val players: StateFlow<List<Player>> = TODO()
-    val settings: StateFlow<RoomSettings> = TODO()
-    val buzzer: SharedFlow<Player> = TODO()
+    init {
+        socket.on(EVENT_PLAYERS_CHANGED) { args -> }
+        socket.on(EVENT_UPDATE_SETTINGS) { args -> }
+        socket.on(EVENT_PLAYER_BUZZER) { args -> }
+    }
+
+    private val _players = MutableStateFlow(emptyList<Player>())
+    val players: StateFlow<List<Player>> = _players
+
+    private val _settings = MutableStateFlow(RoomSettings())
+    val settings: StateFlow<RoomSettings> = _settings
+
+    private val _buzzer = MutableSharedFlow<Player>(
+        extraBufferCapacity = BUZZER_EXTRA_CAPACITY,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val buzzer: SharedFlow<Player> = _buzzer
 
     suspend fun updateSettings(settings: RoomSettings): Unit = TODO()
 
     suspend fun resetBuzzers(): Unit = TODO()
 
     fun destroy(): Unit = TODO()
+
+    override fun toString(): String = "CosmoBuzzRoom(code=$code, url=$url)"
+
+    companion object {
+        private const val EVENT_PLAYERS_CHANGED = "players changed"
+        private const val EVENT_UPDATE_SETTINGS = "update settings"
+        private const val EVENT_PLAYER_BUZZER = "player buzzer"
+        private const val EVENT_RESET_BUZZERS = "reset buzzers"
+
+        private const val BUZZER_EXTRA_CAPACITY = 10
+
+        private const val URL_FMT = "https://www.cosmobuzz.net/#/play/%s"
+    }
 }
 
 sealed class CreateRoomResult

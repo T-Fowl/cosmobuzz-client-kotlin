91e8da06774912f22191ad13128b72216d6636eb
Thomas Fowler <ThomasJamesFowler97@gmail.com>
2021-09-07 18:46:15 +1000

    Added an abstraction of a CosmoSocket with Incoming & Outgoing Events

--- src/main/kotlin/com/tfowl/cosmobuzz/CosmoBuzz.kt
+++ src/main/kotlin/com/tfowl/cosmobuzz/CosmoBuzz.kt
@@ -10,6 +10,25 @@ import kotlinx.coroutines.flow.StateFlow
 import org.json.JSONArray
 import org.json.JSONObject
 
+sealed class IncomingEvent {
+    data class PlayersChanged(val players: List<Player>) : IncomingEvent()
+    data class RoomSettingsChanged(val settings: RoomSettings) : IncomingEvent()
+    data class PlayerBuzzed(val id: String) : IncomingEvent()
+}
+
+sealed class OutgoingEvent {
+    data class UpdateRoomSettings(val settings: RoomSettings) : OutgoingEvent()
+    object ResetBuzzers : OutgoingEvent()
+}
+
+interface CosmoSocket {
+
+    fun receive(callback: (IncomingEvent) -> Unit)
+
+    suspend fun send(event: OutgoingEvent)
+
+    fun disconnect()
+}
 
 class CosmoBuzzRoom(val code: String, private val socket: Socket) {
     val url: String = URL_FMT.format(code)

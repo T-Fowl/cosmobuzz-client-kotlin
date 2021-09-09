80b04e857585425267e4555a93bffdd17effb68d
Thomas <thomasjamesfowler97@gmail.com>
2021-09-09 14:01:16 +1000

    Added a naive implementation of CosmoBuzz::createRoom

--- src/main/kotlin/com/tfowl/cosmobuzz/CosmoBuzz.kt
+++ src/main/kotlin/com/tfowl/cosmobuzz/CosmoBuzz.kt
@@ -1,11 +1,34 @@
 package com.tfowl.cosmobuzz
 
-sealed class CreateRoomResult
+import com.tfowl.socketio.connectAwait
+import com.tfowl.socketio.emitAwait
+import io.socket.client.IO
+import io.socket.engineio.client.transports.WebSocket
 
-data class SuccessfulRoom(val room: CosmoBuzzRoom) : CreateRoomResult()
+sealed class CreateRoomResult {
+    data class Success(val room: CosmoBuzzRoom) : CreateRoomResult()
+    data class Failure(val cause: Throwable) : CreateRoomResult()
+}
 
-data class FailedRoom(val cause: Throwable) : CreateRoomResult()
 
-object CosmoBuzz {
-    suspend fun createRoom(): CreateRoomResult = TODO()
+open class CosmoBuzz {
+    suspend fun createRoom(): CreateRoomResult {
+        return try {
+            val opts = IO.Options().apply {
+                transports = arrayOf(WebSocket.NAME)
+            }
+            val socket = IO.socket(URL, opts).connectAwait()
+
+            val code = socket.emitAwait(CREATE_ROOM).first().toString()
+
+            CreateRoomResult.Success(CosmoBuzzRoom(code, SocketIOCosmoSocket(socket)))
+        } catch (t: Throwable) {
+            CreateRoomResult.Failure(t)
+        }
+    }
+
+    companion object Default : CosmoBuzz() {
+        private const val URL = "https://www.cosmobuzz.net:3000"
+        private const val CREATE_ROOM = "create room"
+    }
 }
